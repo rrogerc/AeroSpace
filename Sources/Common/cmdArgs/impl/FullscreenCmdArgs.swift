959090334a07089a5e9761f4267225804f96088f
Roger Chen <rogerchen803@gmail.com>
2026-03-01 11:12:39 -0600

    Add --width flag to fullscreen command for centered partial-width fullscreen
    Allows specifying a fractional width (0, 1] for fullscreen mode, centering
    the window horizontally. Useful for reading mode where full monitor width
    is too wide.
    
    Usage: fullscreen --width 0.66

--- Sources/Common/cmdArgs/impl/FullscreenCmdArgs.swift
+++ Sources/Common/cmdArgs/impl/FullscreenCmdArgs.swift
@@ -7,6 +7,7 @@ public struct FullscreenCmdArgs: CmdArgs {
         flags: [
             "--no-outer-gaps": trueBoolFlag(\.noOuterGaps),
             "--fail-if-noop": trueBoolFlag(\.failIfNoop),
+            "--width": singleValueSubArgParser(\.width, "<width>") { Double($0).toResult("Can't convert '\($0)' to Double") },
             "--window-id": windowIdSubArgParser(),
         ],
         posArgs: [ArgParser(\.toggle, parseToggleEnum)],
@@ -15,10 +16,13 @@ public struct FullscreenCmdArgs: CmdArgs {
     public var toggle: ToggleEnum = .toggle
     public var noOuterGaps: Bool = false
     public var failIfNoop: Bool = false
+    public var width: Double? = nil
 }
 
 func parseFullscreenCmdArgs(_ args: StrArrSlice) -> ParsedCmd<FullscreenCmdArgs> {
     parseSpecificCmdArgs(FullscreenCmdArgs(rawArgs: args), args)
         .filterNot("--no-outer-gaps is incompatible with 'off' argument") { $0.toggle == .off && $0.noOuterGaps }
+        .filterNot("--width is incompatible with 'off' argument") { $0.toggle == .off && $0.width != nil }
+        .filter("--width must be greater than 0 and less than or equal to 1") { $0.width == nil || (0 < $0.width! && $0.width! <= 1) }
         .filter("--fail-if-noop requires 'on' or 'off' argument") { $0.failIfNoop.implies($0.toggle == .on || $0.toggle == .off) }
 }
